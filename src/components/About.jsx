b6ac8879df1ea362f8bb3c84924da9777fe45b74
Jorge Adarmes <jorgeluismakubex@gmail.com>
2023-07-17 05:00:31 +0000

    add changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,26 +5,33 @@ export default function About(){
         <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hola, soy Reed
-            <br className="hidden lg:inline-block" />Me encanta construir aplicaciones web.
-          </h1>
-          <p className="mb-8 leading-relaxed">
+          
+          <p className="font-medium text-white">Hello, <span className="text-cyan-500">I´am</span></p>
+          <br />
+          <h3 className="text-cyan-500 text-4xl font-bold ">Jorge Adarmes</h3>
+          <h1 className="mt-2 text-white font-medium text-xl ">Frontend React <span className="text-cyan-500  text-brillo font-bold">HOOKS</span> </h1>
+
+          <br/>
+
+
+
+          
+          <p className="mb-8  text-slate-50">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
             laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            Laborum, voluptas natus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus? 
           </p>
-          <div className="flex justify-center">
+          <div className="flex justify-center ">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Trabaja conmigo
+              className=" bg-cyan-500 hover:shadow-cyan-600 transition duration-200 shadow-lg shadow-cyan-500/50  text-white py-2 px-8 rounded-none ">
+              <span className="text-sm font-semibold"> Let´s Talk </span>
             </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              Mira mis trabajos anteriores
-            </a>
+
+            <button className="rounded-none ml-4 inline-flex text-white border-2 border-cyan-600 hover:shadow-cyan-600 transition duration-200 shadow-lg shadow-cyan-500/50 py-2 px-8"> <span className="text-sm font-semibold">Donwload CV</span> </button>
+            
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 dolce">
